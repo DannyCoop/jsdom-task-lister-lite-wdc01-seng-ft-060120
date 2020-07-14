9fd00e143ac8ac0fbf850ba3fb0f313ad2f32d46
steve <stevepesce879@gmail.com>
2020-07-14 18:37:50 -0400

    finished program up to delete button stretch

--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
 document.addEventListener("DOMContentLoaded", (e) => {
- 
+  let form = document.getElementById("create-task-form");  
+  //listener to create table rows from submitted text
+  form.addEventListener("submit", function(e)
+  {      
+    e.preventDefault();  
+    let button = addInputToTable();//just returns a button
+    clearTextBox();
+    //listener that deletes the row from the table on click
+    button.addEventListener("click",function(e)
+    {
+      deleteButton(button);
+    });   
+  });
 });
+//remove entire row that button is in
+function deleteButton(button)
+{
+  button.parentNode.parentNode.remove();
+}
 
+//clears text box from form
+function clearTextBox()
+{
+  document.getElementsByTagName("input")[0].value = "";
+}
+//returns button, not the table or tr
+//creates table with text data
+function addInputToTable()
+{
+  let input = document.getElementsByTagName("input")[0];
+  let tr = document.createElement("tr");
+  //create 2 empty tds
+  let td = [];
+  td[0] = document.createElement("td");
+  td[1] = document.createElement("td");
+  
+  //tds
+  td[0].textContent = input.value;
+  let button = document.createElement("button");
+  button.style.height = '15px'
+  button.className = 'delete-button'  
+  td[1].appendChild(button)
+  
+  //make table
+  table = document.getElementById('table-tasks');
+  //add tr to doc
+  table.appendChild(tr);
+  //add td to tr
+  tr.appendChild(td[0]);
+  tr.appendChild(td[1]);
+  return button;
+}
 
-// 1. rewrite the submit buttons event function with preventDefault()
-// 2. 
-
-let form = document.getElementById("create-task-form")
-let button = document.getElementsByTagName("input")[1]
-
-let listen = button.addEventListener("click", function(e){
-  e.preventDefault();
-  let input = document.getElementsByTagName("input")[0]
-  let li = document.createElement("li");
-  li.textContent = input.value;
-  input.value = ""
-  // console.log(li);
-  document.getElementById("tasks").appendChild(li);
-  // console.log(form)
-  // console.log(button)
-  // console.log(text)
-});
-
-
+//tables worked better for us than ul's
+// //creates li within ul with content of the text input on form
+// function addInputToLi()
+// {
+//   let input = document.getElementsByTagName("input")[0]
+//   let li = document.createElement("li");
+//   li.textContent = input.value;  
+//   document.getElementById("tasks").appendChild(li);
+// }

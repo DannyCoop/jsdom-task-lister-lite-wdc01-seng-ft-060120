74fbe503867c4dac33ceaf0d587b2dc61f5be3fe
DannyCoop <cooperdaniel5141@gmail.com>
2020-07-14 16:56:09 -0400

    we got it doing what we want just not the way they want us to

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+ 
 });
+
+
+// 1. rewrite the submit buttons event function with preventDefault()
+// 2. 
+
+let form = document.getElementById("create-task-form")
+let button = document.getElementsByTagName("input")[1]
+
+let listen = button.addEventListener("click", function(e){
+  e.preventDefault();
+  let input = document.getElementsByTagName("input")[0]
+  let li = document.createElement("li");
+  li.textContent = input.value;
+  input.value = ""
+  // console.log(li);
+  document.getElementById("tasks").appendChild(li);
+  // console.log(form)
+  // console.log(button)
+  // console.log(text)
+});
+
+
